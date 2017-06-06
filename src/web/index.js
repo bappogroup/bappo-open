ec98139d6a8d359758d1b268c9994dd4690ec051
William Feng <william.feng@origin12.com>
2017-06-06 10:06:23 +1000

    Added ScrollView

--- src/web/index.js
+++ src/web/index.js
@@ -3,6 +3,7 @@ export { default as Button } from './Button';
 export { default as GestureView } from '../common/GestureView';
 export * from './Icon';
 export { default as ListView } from '../common/ListView';
+export { default as ScrollView } from '../common/ScrollView';
 export { default as Text } from '../common/Text';
 export { default as TextInput } from '../common/TextInput';
 export { default as View } from '../common/View';
