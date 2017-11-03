745c657adcda291d64e81b02eeec3ceba4a5c0d6
William Feng <william.feng@bappo.com>
2017-11-03 11:45:31 +1100

    Avoid using String.prototype.padStart

--- src/components/TimePicker/constants.js
+++ src/components/TimePicker/constants.js
@@ -1,6 +1,8 @@
+import { padStart } from 'lodash';
+
 export const DEFAULT_TIME_DISPLAY_FORMAT = 'h:mm A';
 export const DEFAULT_TIME_VALUE_FORMAT = 'HH:mm:ss';
 export const HOURS = ['12', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11'];
 export const MINUTES = Array.from({ length: 60 })
-  .map((_unused, index) => String(index).padStart(2, '0'));
+  .map((_unused, index) => padStart(String(index), 2, '0'));
 export const MERIDIEMS = ['AM', 'PM'];
