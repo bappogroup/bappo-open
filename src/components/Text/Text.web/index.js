531a0312a75eed72173f6c0cca10f4714b3970aa
William Feng <william.feng@bappo.com>
2018-05-12 14:16:34 +1000

    Fix Text to support SSR

--- src/components/Text/Text.web/index.js
+++ src/components/Text/Text.web/index.js
@@ -28,20 +28,6 @@ type Props = {
   testID?: string,
 };
 
-// Adding a CSS rule to display non-selectable texts. Those texts
-// will be displayed as pseudo elements to prevent them from being copied
-// to clipboard. It's not possible to style pseudo elements with inline
-// styles, so, we're dynamically creating a <style> tag with the rule.
-if (document && document.head) {
-  const textAsPseudoElement =
-    '[data-text-as-pseudo-element]::before { content: attr(data-text-as-pseudo-element); }';
-  const head = document.head;
-  const style = document.createElement('style');
-  style.type = 'text/css';
-  style.appendChild(document.createTextNode(textAsPseudoElement));
-  head.appendChild(style);
-}
-
 class Text extends React.Component<Props> {
   static childContextTypes = {
     isParentAText: PropTypes.bool,
@@ -59,6 +45,10 @@ class Text extends React.Component<Props> {
     return {
       isParentAText: true,
     };
+  }
+
+  componentDidMount() {
+    require('./addNonSelectableTextStyle');
   }
 
   render() {
