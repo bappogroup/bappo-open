ad49e51ab2c7a8f1876d0bb16a67e1a2615795d2
William Feng <william.feng@bappo.com>
2018-06-29 15:48:53 +1000

    Fix datepicker not showing on safari

--- storybook/storybook-web/.storybook/decorator-centered.js
+++ storybook/storybook-web/.storybook/decorator-centered.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Styles, View } from 'bappo-components';
+import { ScrollView, Styles, View } from 'bappo-components';
 
 const styles = {
   root: {
-    minHeight: '100vh',
-    maxWidth: 680,
-    marginHorizontal: 'auto',
+    height: '100vh',
+  },
+  scrollView: {
+    maxWidth: 1080,
   },
 };
 
 export default function(renderStory) {
-  return <View style={styles.root}>{renderStory()}</View>;
+  return (
+    <View style={styles.root}>
+      <ScrollView style={styles.scrollView}>{renderStory()}</ScrollView>
+    </View>
+  );
 }
