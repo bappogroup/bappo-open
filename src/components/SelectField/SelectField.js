ca27cb537c775ca45710591cb5d550064ed6bb51
Linton Ball <me@lintonball.com>
2018-07-12 12:48:01 +1000

    select field halfway

--- src/components/SelectField/SelectField.js
+++ src/components/SelectField/SelectField.js
@@ -2,18 +2,49 @@
 
 import * as React from 'react';
 import { styled } from '../../apis/Style';
+import TouchableView from '../../primitives/TouchableView';
+import View from '../../primitives/View';
 import TextField from '../input-fields/TextField';
 
 type Props = {
   items: Array<any>,
-  onSelect: () => void,
+  itemToString: any => string,
+  selectItem: any => any,
 };
 
-const SelectField = ({  }: Props) => {
-  return (
-    <TextField label="Test label" onValueChange={e => console.log({ e })} />
-  );
+type State = {
+  value: string,
 };
+
+class SelectField extends React.Component<Props, State> {
+  state = {
+    value: '',
+  };
+
+  render() {
+    const { items, itemToString, selectItem } = this.props;
+    const { value } = this.state;
+
+    return (
+      <View>
+        <TextField
+          label="Test label"
+          onValueChange={newValue => this.setState({ value: newValue })}
+          value={value}
+        />
+        {value &&
+          items.map(item => (
+            <TouchableView
+              key={`list-item-${itemToString(item)}`}
+              onClick={() => selectItem(item)}
+            >
+              {itemToString(item)}
+            </TouchableView>
+          ))}
+      </View>
+    );
+  }
+}
 
 SelectField.defaultProps = {
   items: [],
--- storybook/storybook-web/stories/2-components/SelectField/examples/PropType.js
+++ storybook/storybook-web/stories/2-components/SelectField/examples/PropType.js
@@ -1,16 +1,35 @@
 import React from 'react';
-import { SelectField } from 'bappo-components';
+import { SelectField, View } from 'bappo-components';
 
-const SelectFieldTypeExample = () => {
-  const items = [
-    { value: 'apple' },
-    { value: 'pear' },
-    { value: 'orange' },
-    { value: 'grape' },
-    { value: 'banana' },
-  ];
+class SelectFieldTypeExample extends React.Component {
+  state = {
+    items: [
+      { value: 'apple' },
+      { value: 'pear' },
+      { value: 'orange' },
+      { value: 'grape' },
+      { value: 'banana' },
+    ],
+  };
 
-  return <SelectField items={items} />;
-};
+  itemToString = item => item.value;
+
+  render() {
+    const { items, selectedItem } = this.state;
+
+    return (
+      <View>
+        <p>
+          Selected: {selectedItem ? this.itemToString(selectedItem) : 'none'}
+        </p>
+        <SelectField
+          items={items}
+          itemToString={this.itemToString}
+          selectItem={selectedItem => this.setState({ selectedItem })}
+        />
+      </View>
+    );
+  }
+}
 
 export default SelectFieldTypeExample;
