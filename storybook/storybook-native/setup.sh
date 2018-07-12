9846d11a369e9a82e7bbe3ff4132add55dd6c802
William Feng <william.feng@bappo.com>
2018-07-12 16:19:08 +1000

    Fix dependencies
    Remove downshift. husky should be dev dependency

--- storybook/storybook-native/setup.sh
+++ storybook/storybook-native/setup.sh
@@ -7,7 +7,6 @@ cp -r ../../fonts ./node_modules/bappo-components/fonts
 cp ../../fonts.gradle ./node_modules/bappo-components/
 cp -r ../../glyphmaps ./node_modules/bappo-components/glyphmaps
 cp ../../package.json ./node_modules/bappo-components/
-cp -r ../../node_modules/downshift ./node_modules/downshift
 cp -r ../../node_modules/es6-error ./node_modules/es6-error
 cp -r ../../node_modules/react-recomponent ./node_modules/react-recomponent
 
