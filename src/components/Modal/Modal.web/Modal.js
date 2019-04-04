57a9d05fec290c54e08c154079183dcf383cf84a
Hernus Carelsen <hernus@users.noreply.github.com>
2019-04-05 10:55:15 +1100

    add fullscreen option to modal

--- src/components/Modal/Modal.web/Modal.js
+++ src/components/Modal/Modal.web/Modal.js
@@ -131,6 +131,16 @@ const desktopStyle = ({ layout, placement }) => {
     `;
   }
 
+  if (placement && placement.type === 'fullscreen') {
+    return `
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      border-radius: 0px;
+    `;
+  }
+
   return `
     left: 0;
     right: 0;
--- storybook/storybook-native/storybook/stories/2-components/Modal/examples/Minimal.js
+++ storybook/storybook-native/storybook/stories/2-components/Modal/examples/Minimal.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Button, Modal, View } from 'bappo-components';
+import { Button, Modal, View, Text } from 'bappo-components';
 
 class ModalMinimalExample extends React.Component {
   state = {
     visible: false,
+    fullscreen: true,
   };
 
   render() {
@@ -11,19 +12,31 @@ class ModalMinimalExample extends React.Component {
       <View>
         <Button
           type="primary"
-          onPress={() => this.setState({ visible: true })}
+          onPress={() => this.setState({ visible: true, fullscreen: false })}
           text="Open Modal"
         />
+        <View style={{ height: 10 }} />
+        <Button
+          type="primary"
+          onPress={() => this.setState({ visible: true, fullscreen: true })}
+          text="Open Full Screen Modal"
+        />
+
         <Modal
           onRequestClose={() => this.setState({ visible: false })}
           visible={this.state.visible}
+          placement={this.state.fullscreen ? { type: 'fullscreen' } : null}
         >
-          <View style={{ height: 20, backgroundColor: 'green' }} />
-          <Button
-            type="primary"
-            onPress={() => this.setState({ visible: false })}
-            text="Close Modal"
-          />
+          <View
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+          >
+            <Text> Hello World </Text>
+            <Button
+              type="primary"
+              onPress={() => this.setState({ visible: false })}
+              text="Close Modal"
+            />
+          </View>
         </Modal>
       </View>
     );
