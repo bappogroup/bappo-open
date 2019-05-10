cc93521fbc8a0cea3cf3422fbf8de0a922971fcc
William Feng <william.feng@bappo.com>
2019-05-10 16:50:33 +1000

    Add TimeField

--- src/components/TimePicker/TimePicker.web/index.js
+++ src/components/TimePicker/TimePicker.web/index.js
@@ -4,6 +4,7 @@ import * as React from 'react';
 import moment from 'moment';
 import type Moment from 'moment';
 import PickerWeb from '../../../internals/Picker.web';
+import type { TimePickerProps } from '../types.js.flow';
 import {
   DEFAULT_TIME_DISPLAY_FORMAT,
   DEFAULT_TIME_VALUE_FORMAT,
@@ -11,63 +12,9 @@ import {
 import WheelPicker from './WheelPicker';
 import { PlaceholderText, ValueText } from './StyledComponents';
 
-type Props = {
-  /**
-   * Overrides the text that's read by the screen reader when the user interacts with the element.
-   */
-  accessibilityLabel?: string,
-  /**
-   * If `true`, focuses the input on `componentDidMount`. The default value is `false`.
-   */
-  autoFocus: ?boolean,
+type Props = TimePickerProps & {
+  // Will be removed
   className?: string,
-  /**
-   * If `true`, the input value can be cleared by pressing a button. The default value is `true`.
-   */
-  clearable: ?boolean,
-  clearValueText: string,
-  /**
-   * Date format of the displayed value.
-   */
-  displayFormat: string,
-  /**
-   * Callback that is called when the input is blurred.
-   */
-  onBlur?: ?() => void,
-  /**
-   * Callback that is called when the input is focused.
-   */
-  onFocus?: ?() => void,
-  /**
-   * Callback that is called when the input value changes.
-   */
-  onValueChange?: ?(value: string | null) => void,
-  /**
-   * The string that will be rendered when there is no value.
-   */
-  placeholder: string,
-  /**
-   * If `true`, the input is not editable. The default value is `false`.
-   */
-  readOnly: boolean,
-  /**
-   * Function to render the dropdown icon.
-   */
-  renderDropdownIcon?: ?() => ?React.Element<any>,
-  // TODO
-  style?: any,
-  /**
-   * Used to locate this view in end-to-end tests.
-   */
-  testID?: string,
-  /**
-   * The value of the time input.
-   */
-  value?: ?string,
-  /**
-   * Date format of the input value.
-   */
-  valueFormat: string,
 };
 
 class TimePicker extends React.Component<Props> {
--- src/components/input-fields/index.js
+++ src/components/input-fields/index.js
@@ -5,3 +5,4 @@ export { default as SwitchField } from './SwitchField';
 export { default as TextField } from './TextField';
 export { default as DatePickerField } from './DatePickerField';
 export { default as SelectField } from './SelectField';
+export { default as TimeField } from './TimeField';
