cf293648365ed9e78f6f0ab10b22f3bf86b3db0c
William Feng <william.feng@bappo.com>
2019-05-10 17:31:50 +1000

    Update story books

--- storybook/storybook-web/.storybook/webpack.config.js
+++ storybook/storybook-web/.storybook/webpack.config.js
@@ -14,17 +14,18 @@ module.exports = (baseConfig, env, defaultConfig) => {
       __dirname,
       '../node_modules/@storybook/react',
     ),
-    'bappo-components': path.join(__dirname, '../../../'),
-    react: path.join(__dirname, '../../../node_modules/react'),
-    'react-dom': path.join(__dirname, '../../../node_modules/react-dom'),
-    'react-dom/unstable-native-dependencies': path.join(
-      __dirname,
-      '../../../node_modules/react-dom/unstable-native-dependencies',
-    ),
-    'styled-components': path.join(
-      __dirname,
-      '../../../node_modules/styled-components',
-    ),
+    // only use the following for development
+    // 'bappo-components': path.join(__dirname, '../../../'),
+    // react: path.join(__dirname, '../../../node_modules/react'),
+    // 'react-dom': path.join(__dirname, '../../../node_modules/react-dom'),
+    // 'react-dom/unstable-native-dependencies': path.join(
+    //   __dirname,
+    //   '../../../node_modules/react-dom/unstable-native-dependencies',
+    // ),
+    // 'styled-components': path.join(
+    //   __dirname,
+    //   '../../../node_modules/styled-components',
+    // ),
   };
 
   return defaultConfig;
