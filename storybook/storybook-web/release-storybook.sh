db736b6a0baf75c672240845c365ef6f3cb16002
Stan <stanley.luo@bappo.com>
2019-05-27 16:52:40 +1000

    Upgrade bappo-components in storybook

--- storybook/storybook-web/release-storybook.sh
+++ storybook/storybook-web/release-storybook.sh
@@ -1,5 +1,20 @@
+# Upgrade bappo-components version in storybook
 yarn upgrade bappo-components --latest
 yarn --cwd storybook/storybook-native upgrade bappo-components --latest
 git add -A
 git commit -m "Upgrade bappo-components in storybook"
 git push origin
+
+# Build storybook
+yarn build
+
+# Deploy storybook in another branch
+git checkout gh-pages
+rm -rf ../../storybook-web
+mv dist ../../storybook-web
+git add -A
+git commit -m \"Storybook deploy\"
+git push origin gh-pages
+
+# Back to master
+git checkout -
