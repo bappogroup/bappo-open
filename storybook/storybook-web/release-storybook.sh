e7a32f4342e797af8c32a0ed3aed9568564aa9a6
William Feng <william.feng@bappo.com>
2019-07-05 17:43:30 +1000

    Upgrade bappo-components in storybook

--- storybook/storybook-web/release-storybook.sh
+++ storybook/storybook-web/release-storybook.sh
@@ -1,6 +1,6 @@
 # Upgrade bappo-components version in storybook
 yarn upgrade bappo-components --latest
-yarn --cwd storybook/storybook-native upgrade bappo-components --latest
+yarn --cwd ../storybook-native upgrade bappo-components --latest
 git add -A
 git commit -m "Upgrade bappo-components in storybook"
 git push origin
