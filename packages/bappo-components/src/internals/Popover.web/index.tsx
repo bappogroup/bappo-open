b91c8f0e4d4148a2977b6eb306e89817955e4502
William Feng <william.feng@bappo.com>
2020-11-19 16:40:45 +1100

    Handle window resize

--- packages/bappo-components/src/internals/Popover.web/index.tsx
+++ packages/bappo-components/src/internals/Popover.web/index.tsx
@@ -14,21 +14,35 @@ export function Popover({
 }: PopoverProps) {
   const contentContainerRef = React.useRef<HTMLDivElement>(null);
 
+  const updateContentContainerStyle = React.useCallback(() => {
+    if (!anchorEl || !contentContainerRef.current) return;
+    const { top, left } = calculateContentContainerStyle(
+      anchorEl,
+      contentContainerRef.current,
+    );
+    contentContainerRef.current.style.top = `${top}px`;
+    contentContainerRef.current.style.left = `${left}px`;
+    // Now that we know the exact position of the popup, we can show it
+    contentContainerRef.current.style.opacity = '1';
+  }, [anchorEl]);
+
+  // Update popup position upon open
   React.useEffect(() => {
-    if (visible && anchorEl && contentContainerRef.current) {
-      const { top, left } = calculateContentContainerStyle(
-        anchorEl,
-        contentContainerRef.current,
-      );
-      if (top != null) {
-        contentContainerRef.current.style.top = `${top}px`;
-      }
-      if (left != null) {
-        contentContainerRef.current.style.left = `${left}px`;
-      }
-      contentContainerRef.current.style.opacity = '1';
+    if (visible) {
+      updateContentContainerStyle();
     }
-  }, [anchorEl, visible]);
+  }, [updateContentContainerStyle, visible]);
+
+  // Handle window resize
+  React.useEffect(() => {
+    const handleResize = () => {
+      updateContentContainerStyle();
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [updateContentContainerStyle]);
 
   return (
     <Overlay color="transparent" onPress={onRequestClose} visible={visible}>
@@ -46,8 +60,8 @@ function calculateContentContainerStyle(
   anchorEl: HTMLElement,
   contentContainerEl: HTMLElement,
 ): {
-  top?: number;
-  left?: number;
+  top: number;
+  left: number;
 } {
   const anchorRect = anchorEl.getBoundingClientRect();
   const contentContainerRect = contentContainerEl.getBoundingClientRect();
@@ -71,5 +85,6 @@ const ContentContainer = styled(DivViewBase)`
   background-color: white;
   border: 1px solid #ccc;
   box-shadow: 0 1px 0 rgba(0, 0, 0, 0.06);
+  // hide popup initially until we know its exact position
   opacity: 0;
 `;
