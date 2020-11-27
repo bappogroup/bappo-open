154607c2f4b575a8a264ebed0b6e2a49540f3837
emil-bappo <emil@bappo.com>
2020-11-27 13:56:09 +0200

    #1344 IOS Card style - shadow radius units

--- packages/bappo-components/src/components/Card/styles.ios.ts
+++ packages/bappo-components/src/components/Card/styles.ios.ts
@@ -3,5 +3,5 @@ import { css } from 'styled-components';
 export const shadowStyle = css`
   shadow-color: rgba(0, 0, 0, 0.16);
   shadow-offset: 0 2px;
-  shadow-radius: 3;
+  shadow-radius: 3px;
 `;
