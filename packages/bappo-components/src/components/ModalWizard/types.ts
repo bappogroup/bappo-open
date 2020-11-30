310b97b2559932e11f47d304b621ab5bdc80e266
emil-bappo <emil@bappo.com>
2020-11-30 13:02:51 +0200

    #1370 Initial conversion of ModalWizard to TypeScript and functional component

---
+++ packages/bappo-components/src/components/ModalWizard/types.ts
@@ -0,0 +1,27 @@
+import { ReactElement } from 'react';
+
+import { ModalProps } from '../Modal/types';
+
+export type ModalWizardState = {
+  step: number;
+  submitting: boolean;
+  formValues: any;
+};
+
+export type ModalWizardProps = {
+  startFromStep: number;
+  onFinish: () => void;
+  onRequestClose: () => void;
+  screens: Array<ModalWizardScreen>;
+  submitButtonText?: string;
+  canSubmit?: boolean;
+  visible?: boolean;
+  placement?: ModalProps['placement'];
+  testID?: string;
+};
+
+export type ModalWizardScreen = {
+  title?: string;
+  render: () => ReactElement;
+  isForm?: boolean;
+};
