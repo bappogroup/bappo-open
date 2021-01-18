fbd16c4709f42660e62feb8532f7e8a3e80cc015
William Feng <william.feng@bappo.com>
2021-01-18 19:06:57 +1100

    Fix 1426 icon selector crashes in form and improve performance (#69)
    * Use standalone mode if name prop is missing
    
    * Add debouncing to avoid jank

--- packages/bappo-components/src/components/IconSelector/IconModal/IconModal.web.tsx
+++ packages/bappo-components/src/components/IconSelector/IconModal/IconModal.web.tsx
@@ -1,13 +1,12 @@
-import chunk from 'lodash/chunk';
 import React, { useMemo, useState } from 'react';
 import styled from 'styled-components';
 
 import MateriIcon from '../../../../glyphmaps/MaterialIcons.json';
-import TextField from '../../../components/input-fields/TextField';
-import FlatList from '../../../primitives/FlatList';
+import { useDebouncedInputProps } from '../../../internals/hooks/useDebouncedInputProps';
 import View from '../../../primitives/View';
-import IconButton from '../../IconButton';
+import TextField from '../../input-fields/TextField';
 import Modal from '../../Modal';
+import { IconList } from './IconList';
 
 interface IconModalProps {
   modalVisable: boolean;
@@ -22,17 +21,17 @@ const IconModal: React.FC<IconModalProps> = ({
   setModalVisable,
   setIconName,
 }) => {
-  const ARRAY_SIZE = 23;
   const [input, setInput] = useState('');
-  // filter the list and generate 2D array for flatList
-  const resultArray = useMemo(
-    () =>
-      chunk(
-        iconList.filter((icon) => (input ? icon.includes(input) : true)),
-        ARRAY_SIZE,
-      ),
+
+  const selectedIcons = useMemo(
+    () => iconList.filter((icon) => (input ? icon.includes(input) : true)),
     [input],
   );
+
+  const textFieldProps = useDebouncedInputProps({
+    onValueChange: setInput,
+    value: input,
+  });
 
   return (
     <Modal
@@ -42,34 +41,17 @@ const IconModal: React.FC<IconModalProps> = ({
     >
       <IconsContainer>
         <TextField
-          value={input}
-          onValueChange={setInput}
+          {...textFieldProps}
           placeholder="Type to search Icon"
           reserveErrorSpace={false}
         />
-        <FlatListContainer>
-          <FlatList
-            horizontal={false}
-            data={resultArray}
-            renderItem={({ item }: { item: string[] }) => (
-              <FlatListRow key={item[0]}>
-                {item.map((icon: string) => {
-                  return (
-                    <IconButton
-                      key={icon}
-                      name={icon}
-                      onPress={() => {
-                        setIconName(icon);
-                        setModalVisable(false);
-                      }}
-                      tooltip={icon}
-                    />
-                  );
-                })}
-              </FlatListRow>
-            )}
-          />
-        </FlatListContainer>
+        <IconList
+          onSelect={(icon: string) => {
+            setIconName(icon);
+            setModalVisable(false);
+          }}
+          selectedIcons={selectedIcons}
+        />
       </IconsContainer>
     </Modal>
   );
@@ -77,15 +59,6 @@ const IconModal: React.FC<IconModalProps> = ({
 
 const IconsContainer = styled(View)`
   padding: 0px 8px;
-`;
-
-const FlatListContainer = styled(View)`
-  align-items: center;
-  height: 145px;
-`;
-
-const FlatListRow = styled(View)`
-  flex-direction: row;
 `;
 
 export default IconModal;
